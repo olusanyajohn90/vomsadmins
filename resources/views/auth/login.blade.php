76a2966d3f3814e970e5e064354a325189078e1c
olusanyajohn90 <olusanya.john90@gmail.com>
2022-12-20 14:52:54 +0100

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -163,7 +163,7 @@
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                        
                                     </a>
                                 @endif
                             </form>
